505825ee13a9f0b5ff896691835f0733a334e5d0
Robert Fuchs <RobertFuchs97@gmail.com>
2019-09-03 17:21:35 +0200

    Implement more methods of wmctrl

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
 use std::process::Command;
+use std::process::Output;
 
+
+// pub mod transformation;
 pub mod transformation;
 pub mod state;
 pub mod window;
 
+use transformation::Transformation;
+use state::State;
+use window::Window;
 
-pub fn help() -> std::process::Output {
+
+pub fn help() -> Output {
     wmctrl("-h")
 }
 
-pub fn list_windows() -> Vec<window::Window> {
+pub fn list_windows() -> Vec<Window> {
     let output_table = String::from_utf8(wmctrl("-l").stdout)
         .unwrap();
 
@@ -27,7 +34,7 @@ pub fn list_windows() -> Vec<window::Window> {
         let mut title = String::from("");
         title_substrings.iter().for_each(|&e| title += e);
 
-        windows.push(window::Window::new(String::from(columns[0]),
+        windows.push(Window::new(String::from(columns[0]),
             String::from(columns[1]),
             String::from(columns[2]),
             title
@@ -38,60 +45,70 @@ pub fn list_windows() -> Vec<window::Window> {
 }
 
 /// This equals the -m flag
-pub fn show_wm_information() -> std::process::Output {
+pub fn show_wm_information() -> Output {
     wmctrl("-m")
 }
 
 /// This equals the -d flag
-pub fn list_desktops() -> std::process::Output {
+pub fn list_desktops() -> Output {
     wmctrl("-d")
 }
 
 /// This equals the -s flag
 /// desktop usually means workspace in this context
-pub fn switch_desktop(desktop: &str) -> std::process::Output {
+pub fn switch_desktop(desktop: &str) -> Output {
     wmctrl(&format!("-s {}", desktop))
 }
 
 /// window: the window id or a string that matches part of the title
-pub fn activate_window(window: &window::Window) -> std::process::Output {
+pub fn activate_window(window: &Window) -> Output {
     let args = format!("-a {}", window.get());
     wmctrl(&args)
 }
 
-pub fn close_window(window: &window::Window) -> std::process::Output {
+pub fn close_window(window: &Window) -> Output {
     let args = format!("-c {}", window.get());
     wmctrl(&args)
 }
 
 /// Moves the window to the current desktop and raises it
-pub fn move_window_to_current_desktop(window: &window::Window) -> std::process::Output {
+pub fn move_window_to_current_desktop(window: &Window) -> Output {
     let args = format!("-R {}", window.get());
     wmctrl(&args)
 }
 
 /// Moves the window to the specified desktop
-pub fn move_window(window: &window::Window, desktop: &str) -> std::process::Output {
+pub fn move_window(window: &Window, desktop: &str) -> Output {
     let args = format!("-r {} -t {}", window.get(), desktop);
     wmctrl(&args)
 }
 
-pub fn move_and_resize(window: &window::Window, transformation: transformation::Transformation) -> std::process::Output {
+pub fn move_and_resize(window: &Window, transformation: Transformation) -> Output {
     let args = format!("-r {} -e {}", window.get(), transformation);
     wmctrl(&args)
 }
 
-pub fn change_state(window: &window::Window, state: state::State) -> std::process::Output {
+pub fn change_state(window: &Window, state: State) -> Output {
     let args = format!("-r {} -b {}", window.get(), state);
     wmctrl(&args)
 }
 
-pub fn set_long_title(window: &window::Window, title: &str) -> std::process::Output {
+pub fn set_long_title(window: &Window, title: &str) -> Output {
     let args = format!("-r {} -N {}", window.get(), title);
     wmctrl(&args)
 }
 
-fn wmctrl(args: &str) -> std::process::Output {
+pub fn set_short_title(window: &Window, title: &str) -> Output {
+    let args = format!("-r {} -I {}", window.get(), title);
+    wmctrl(&args)
+}
+
+pub fn set_both_title(window: &Window, title: &str) -> Output {
+    let args = format!("-r {} -T {}", window.get(), title);
+    wmctrl(&args)
+}
+
+fn wmctrl(args: &str) -> Output {
      Command::new("sh")
         .arg("-c")
         .arg(format!("wmctrl {}", args))
