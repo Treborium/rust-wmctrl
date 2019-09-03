f1a6f9d007f1f99300c3919f34ff2be9a52b012e
Robert Fuchs <RobertFuchs97@gmail.com>
2019-09-03 12:34:21 +0200

    Refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -30,64 +30,34 @@ pub fn switch_desktop(desktop: &str) -> std::process::Output {
 
 /// window: the window id or a string that matches part of the title
 pub fn activate_window(window: &str) -> std::process::Output {
-    let mut args = format!("-a {}", window);
-
-    if window.starts_with("0x") {
-        args += " -i";
-    }
-
+    let args = format!("-a {}", parse_window(window));
     wmctrl(&args)
 }
 
 pub fn close_window(window: &str) -> std::process::Output {
-     let mut args = format!("-c {}", window);
-
-    if window.starts_with("0x") {
-        args += " -i";
-    }
-
+    let args = format!("-c {}", parse_window(window));
     wmctrl(&args)
 }
 
 /// Moves the window to the current desktop and raises it
 pub fn move_window_to_current_desktop(window: &str) -> std::process::Output {
-    let mut args = format!("-R {}", window);
-
-    if window.starts_with("0x") {
-        args += " -i";
-    }
-
+    let args = format!("-R {}", parse_window(window));
     wmctrl(&args)
 }
 
 /// Moves the window to the specified desktop
 pub fn move_window(window: &str, desktop: &str) -> std::process::Output {
-    let mut args = format!("-r {} -t {}", window, desktop);
-
-    if window.starts_with("0x") {
-        args += " -i";
-    }
-
+    let args = format!("-r {} -t {}", parse_window(window), desktop);
     wmctrl(&args)
 }
 
 pub fn move_and_resize(window: &str, transformation: transformation::Transformation) -> std::process::Output {
-    let mut args = format!("-r {} -e {}", window, transformation);
-
-    if window.starts_with("0x") {
-        args += " -i";
-    }
-
+    let args = format!("-r {} -e {}", parse_window(window), transformation);
     wmctrl(&args)
 }
 
 pub fn change_state(window: &str, state: state::State) -> std::process::Output {
-    let mut args = format!("-r {} -b {}", window, state);
-
-    if window.starts_with("0x") {
-        args += " -i";
-    }
-
+    let args = format!("-r {} -b {}", parse_window(window), state);
     wmctrl(&args)
 }
 
@@ -97,6 +67,14 @@ fn wmctrl(args: &str) -> std::process::Output {
         .arg(format!("wmctrl {}", args))
         .output()
         .expect(&format!("failed to execute 'wmctrl {}'", args))
+}
+
+fn parse_window(window: &str) -> String {
+    if window.starts_with("0x") {
+        format!("{} -i", window)
+    } else {
+        window.to_owned()
+    }
 }
 
 #[cfg(test)]
