d9d01d7ff7187ff460447ad81137745e794a4b75
Robert Fuchs <RobertFuchs97@gmail.com>
2019-09-03 18:43:49 +0200

    Add the current transformation of the window as an field

--- src/window.rs
+++ src/window.rs
@@ -1,17 +1,22 @@
-#[derive(Debug)]
+use crate::transformation::Transformation;
+
 pub struct Window {
-    pub id: String,
+    pub id: Option<String>,
     pub desktop: String,
     pub client_machine: String,
     pub title: String,
+    pub transformation: Transformation,
 }
 
 impl Window {
-    pub fn new(id: String, desktop: String, client_machine: String, title: String) -> Window {
-        Window { id, desktop, client_machine, title }
+    pub fn new(id: Option<String>, desktop: String, client_machine: String, title: String, transformation: Transformation) -> Window {
+        Window { id, desktop, client_machine, title, transformation }
     }
 
     pub fn get(&self) -> String {
-        format!("{} -i", self.id)
+        match &self.id {
+            Some(id) => format!("{} -i", id).clone(),
+            None => self.title.clone(),
+        }
     }
 }
