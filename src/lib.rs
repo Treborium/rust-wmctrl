5b0d20e30040216758650455f8c42d94d2623034
Robert Fuchs <RobertFuchs97@gmail.com>
2019-09-03 11:29:19 +0200

    Implement method for -l and -m flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
 use std::process::Command;
-use std::str;
 
-pub fn print_windows() -> std::process::Output {
-    Command::new("sh")
-        .arg("-c")
-        .arg("wmctrl -l")
-        .output()
-        .expect("failed to execute 'wmctrl -l'")
+pub fn list_windows() -> std::process::Output {
+   wmctrl("-l")
 }
 
+/// This equals the -m flag
+pub fn show_information_about_wm() -> std::process::Output {
+    wmctrl("-m")
+}
+
+fn wmctrl(args: &str) -> std::process::Output {
+     Command::new("sh")
+        .arg("-c")
+        .arg(format!("wmctrl {}", args))
+        .output()
+        .expect(&format!("failed to execute 'wmctrl {}'", args))
+}
 
 #[cfg(test)]
 mod tests {
